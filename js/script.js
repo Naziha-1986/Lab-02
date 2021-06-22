e75751ae2efece26f1b26812ff84febe9cccf862
Naziha-1986 <nazihaqawasmeh@gmail.com>
2021-06-22 19:33:51 +0300

    add score

--- js/script.js
+++ js/script.js
@@ -35,103 +35,117 @@
 
 
 
- let userCountry = prompt('am i come from america? yes or no');
+let userCountry = prompt('am i come from Jordan? yes or no');
 if (userCountry.toLowerCase() == "yes") {
-     alert('wrong answer!!!');
+    alert('right answer!!!');
     /*console.log('i am from america',userCountry);*/
- } else {
-     alert('right answer');
-     /*console.log('i am from america',userCountry);*/
+} else {
+    alert('wrong answer');
+    /*console.log('i am from america',userCountry);*/
 }
 
-// let userReligin = prompt('am i muslim? yes or no');
-// if (userReligin.toLowerCase() == "no") {
-//     alert('wrong answer!!!');
-//     /*console.log('i am  muslim',userReligin);*/
-// } else {
-//     alert('right answer');
-//     /*console.log('i am  muslim',userReligin);*/
-// }
+let userReligin = prompt('am i muslim? yes or no');
+if (userReligin.toLowerCase() == "yes") {
+    alert('right answer!!!');
+    /*console.log('i am  muslim',userReligin);*/
+} else {
+    alert('wrong answer');
+    /*console.log('i am  muslim',userReligin);*/
+}
 
 
 
-// let userStatus = prompt('am i married? yes or no');
-// if (userStatus.toLowerCase() == "yes") {
-//     alert('right answer!!!');
-//     /*console.log('i am married',userStatus);*/
-// } else {
-//     alert('wrong answer');
-//     /*console.log('i am married',userStatus);*/
-// }
+//  let userStatus = prompt('am i married? yes or no');
+//  if (userStatus.toLowerCase() == "yes") {
+//      alert('right answer!!!');
+//      /*console.log('i am married',userStatus);*/
+//  } else {
+//      alert('wrong answer');
+//      /*console.log('i am married',userStatus);*/
+//  }
 
 // let userFood = prompt('is Shawerma my favorite food ? yes or no');
-// if (userStatus.toLowerCase() == "yes") {
-//     alert('wrong answer!!!');
-//     /*console.log('favorite food is shawerma',userFood);*/
-// } else {
-//     alert('right answer');
-//     /*console.log('favorite food is shawerma',userFood);*/
-// }
+//  if (userStatus.toLowerCase() == "yes") {
+//      alert('right answer!!!');
+//      /*console.log('favorite food is shawerma',userFood);*/
+//  } else {
+//      alert('wrong answer');
+//      /*console.log('favorite food is shawerma',userFood);*/
+//  }
 
-// let userBook = prompt('is one hundrer years of solitude my favorite book ? yes or no');
-// if (userStatus.toLowerCase() == "yes") {
-//     alert('right answer!!!');
-//     /*console.log('favorite food is shawerma',userBook);*/
-// } else {
-//     alert('wrong answer');
+//  let userBook = prompt('is one hundrer years of solitude my favorite book ? yes or no');
+//  if (userStatus.toLowerCase() == "yes") {
+//      alert('right answer!!!');
+//      /*console.log('favorite food is shawerma',userBook);*/
+//  } else {
+//      alert('wrong answer');
 //     /*console.log('one hundrer years of solitude is my favorite book',userBook);*/
-// }
+//  }
 
-// alert('Welcome again  ' + yourName);
-
-
+//  alert('Welcome again  ' + yourName);
 
 
 
-// let luckyNumber = prompt('guess what is my luckey number?');
-// luckyNumber = Number(luckyNumber);
-// for (let i = 0; i <= 2; i++) {
+
+
+//  let luckyNumber = prompt('guess what is my luckey number?');
+//  luckyNumber = Number(luckyNumber);
+//  for (let i = 0; i <= 2; i++) {
 //     if (luckyNumber === 14) {
-//         alert("it is right");
+//          alert("it is right");
 //     } else if (luckyNumber > 14) {
-
-//         alert('too high');
-
+//          alert('too high');
+//          luckyNumber = prompt('guess what is my luckey number?');
+//         luckyNumber = Number(luckyNumber);
+//      } else {
+//          alert('too low');
 //         luckyNumber = prompt('guess what is my luckey number?');
 //         luckyNumber = Number(luckyNumber);
+//     }
+//  }
 
+
+
+// let drinks = ['cola', 'coffee', 'tea', 'water'];
+// let drinkType = prompt('guess what are my fav drink?');
+// for (let i = 0; i <= drinks.length; i++) {
+//     if (drinkType === drinks[0] || drinkType === drinks[1] || drinkType === drinks[2] || drinkType === drinks[3]) {
+//         alert('right answer');
+//         break;
 //     } else {
-//         alert('too low');
-//         luckyNumber = prompt('guess what is my luckey number?');
-//         luckyNumber = Number(luckyNumber);
+//         alert('guess again');
+//         drinkType = prompt("guess what are my fav drink");
+
 
 //     }
+
+// }
+// alert("by the way, my favorite drink is:'cola', 'coffee', 'tea', 'water'");
+
+
+
+
+
+let score = 0
+    if(userCountry === yes && userReligin === yes && userFood === yes && userStatus === yes &&userBook===yes) {
+        alert('score =', score +5, '/5');
+} else if (userCountry === no && userReligin === no && userFood === no && userStatus === no &&userBook===no){
+    alert('score =', score, '/5');
+}else if(userCountry === yes && userReligin ===yes && userFood === no && userStatus === no &&userBook===no
+
+)
+    alert('score=', score[1], '/1')
+}
+if (userReligin === "yes") {
+    alert('score=', score[3], "/2");
+} else {
+    alert('score=', score[1], '/2')
+}
+}
+// if()
 // }
 
-
-
-let drinks = ['cola', 'coffee', 'tea', 'water'];
-let drinkType = prompt('guess what are my fav drink?');
-for (let i = 0; i <= drinks.length; i++) {
-    if (drinkType === drinks[0] || drinkType === drinks[1] || drinkType === drinks[2] || drinkType === drinks[3]) {
-        alert('right answer');
-
-
-        break;
-    } else {
-        alert('guess again');
-        drinkType = prompt("guess what are my fav drink");
-
-
-    }
-
-}
-alert("by the way, my favorite drink is:'cola', 'coffee', 'tea', 'water'");
-
-
-let score =[0,1,2,3,4,5,6,7]
-
-for (let i =0; i <= score.length; i++){
-if(score===)
-
-}
+// if {userReligin==="yes"&& userFood==="yes"&&userStatus==="yes"&&userBook==="yes")
+//     {
+//     alert(score=score);
+// }
